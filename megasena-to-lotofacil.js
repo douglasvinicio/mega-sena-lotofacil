6f003003427ff873e007b85b5195cf372b29656f
douglasvinicio <douglas.vinicio@hotmail.com>
2022-12-22 13:32:07 -0300

    Done

--- megasena-to-lotofacil.js
+++ megasena-to-lotofacil.js
@@ -7,10 +7,6 @@
 // - Used sort and filter for the sake of verbose
 // - If the final array doesn't have a a total of 15 numbers it generated a random number from 1 to 25 that is not already on the array. 
 
-// Linkedin Post :
-// Changing careers and job hunting in tech is not always evident, but following advice from some senior developers that I met since I'm involved in the community I started to search for simple small projects to help people around me and use real world examples to develop a tool. It hits different than following a tutorial or a course of how to create xyz application using this tool. Here you're creating something totally new, off course it will have research and ctrl+c ctrl+v from you know overflow but still you will have to think on not just how to solve a unique problem but also how to approach it to save time and resources, a bit of project management since you can face it as something serious worth of a place on your portfolio or an live app that you can brag about when asked what you do for a living.   
-// Took a real case scenario from a task given to me from my mother, instead of doing by hand I decided to use my coding skills to automate and reduce margin to error
-
 const firstBet = [03 ,13, 29, 34, 35, 27]
 const secondBet = [08 ,18, 28, 37, 42, 53]
 const thirdBet = [01 ,02, 05, 04, 06, 07]
